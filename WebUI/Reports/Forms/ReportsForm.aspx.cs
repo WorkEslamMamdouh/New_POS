54e0428d8511be2cc5bad7b4899c16553ddc18c4
WorkEslamMamdouh <81823203+WorkEslamMamdouh@users.noreply.github.com>
2021-10-28 21:38:09 +0200

    Eslam♥

--- WebUI/Reports/Forms/ReportsForm.aspx.cs
+++ WebUI/Reports/Forms/ReportsForm.aspx.cs
@@ -44,7 +44,11 @@ namespace RS.WebUI.Reports.Forms
 
 
         //string cs = "Data Source=SQL5061.site4now.net; database =db_a7882d_apieltawhed; user id =db_a7882d_apieltawhed_admin; Password=619619Ss619619";
-        string cs = "Data Source= . ; database = POS2021 ; user id = sa ; Password= 619619 ;";
+        //string cs = "Data Source= . ; database = POS2021 ; user id = sa ; Password= 619619 ;";
+        string cs = "Data Source= SQL5077.site4now.net ; database = db_a7b97c_pos ; user id = db_a7b97c_pos_admin ; Password= 619619Ss619619 ;";
+
+
+           //"Data Source=SQL5077.site4now.net;Initial Catalog=db_a7b97c_pos;User Id=db_a7b97c_pos_admin;Password=YOUR_DB_PASSWORD-->
 
         string Par;
 
